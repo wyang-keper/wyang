983b1bf57ad6a73cc0b58f4058bab0cfecd13cd1
wyang-keper <80016482+wyang-keper@users.noreply.github.com>
2021-06-17 20:51:47 +0800

    备忘录模式

---
+++ Memento Pattern/src/Caretaker/MemoManager.java
@@ -0,0 +1,14 @@
+package Caretaker;
+
+import Memento.MemoBean;
+
+public class MemoManager {
+	MemoBean memento;
+	  public MemoBean getMemento() {
+	    return memento;
+	  }
+	  public void setMemento(MemoBean memento) {
+	    this.memento = memento;
+	  }
+
+}
