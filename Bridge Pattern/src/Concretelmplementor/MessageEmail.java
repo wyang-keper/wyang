7a76d1a76d593c0663427383e420166ae7074070
wyang-keper <80016482+wyang-keper@users.noreply.github.com>
2021-06-17 19:42:51 +0800

    桥接模式

---
+++ Bridge Pattern/src/Concretelmplementor/MessageEmail.java
@@ -0,0 +1,12 @@
+package Concretelmplementor;
+
+import Implementor.MessageImplementor;
+
+public class MessageEmail implements MessageImplementor {
+
+    @Override
+    public void send(String message, String toUser) {
+        System.out.println("使用邮件短消息的方法，发送消息'"+message+"'给"+toUser);
+    }
+
+}
---
+++ Bridge Pattern/src/RefinedAbstraction/CommonMessage.java
@@ -0,0 +1,16 @@
+package RefinedAbstraction;
+
+import Abstraction.AbstractMessage;
+import Implementor.MessageImplementor;
+
+public class CommonMessage extends AbstractMessage {
+
+    public CommonMessage(MessageImplementor impl) {
+        super(impl);
+    }
+    @Override
+    public void sendMessage(String message, String toUser) {
+        // 对于普通消息，直接调用父类方法，发送消息即可
+        super.sendMessage(message, toUser);
+    }
+}
